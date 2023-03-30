f9a6d816f98ab5882a60b8134460c91ec60c1be8
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2023-03-30 11:26:04 +0530

    Create Rectangle_Area.cpp

---
+++ Rectangle_Area.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle{
+    public:
+    int width;
+    int height;
+     void display(){
+        cout<<width<<" "<<height<<endl;
+    }
+};
+class RectangleArea: public Rectangle{
+    public:
+    void read_input(){
+       cin>>width>>height;
+    }
+    void display(){
+         cout<<width*height;
+    }
+};
