63605e10ea7ee28672e3d9d7d18f3b1e463d187b
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2023-04-02 17:39:59 +0530

    Create Basic_Data_Types.cpp

---
+++ Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+     int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+     cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e<<endl;
+    return 0;
+}
