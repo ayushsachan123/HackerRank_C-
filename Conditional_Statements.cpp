62b5710c4fafe9b9fec5064149368d40043bf967
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2023-04-02 17:40:55 +0530

    Create Conditional_Statements.cpp

---
+++ Conditional_Statements.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // Write your code here
+    int a;
+    cin>>a;
+    if(a==1){
+      cout<<"one";  
+    }
+    else if(a==2){
+        cout<<"two";
+    }
+    else if(a==3){
+        cout<<"three";
+    }
+    else if(a==4){
+        cout<<"four";
+    }
+    else if(a==5){
+        cout<<"five";
+    }
+    else if(a==6){
+        cout<<"six";
+    }
+    else if(a==7){
+        cout<<"seven";
+    }
+    else if(a==8){
+        cout<<"eight";
+    }
+    else if(a==9){
+        cout<<"nine";
+    }
+    else if(a>9){
+        cout<<"Greater than 9";
+    }
+
+    return 0;
+}
+
