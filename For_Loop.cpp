dfaf82a41003c357536cf934ae433841f5290297
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2023-04-02 18:07:09 +0530

    Create For_Loop.cpp

---
+++ For_Loop.cpp
@@ -0,0 +1,67 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+     int n,m;
+    cin >> n>>m;
+    for(int i=n;i<=m;i++){
+    if(i == 1) {
+        cout << "one"<<endl;
+    }
+    else if(i == 2) {
+        cout << "two"<<endl;
+    }
+    else if(i == 3) {
+        cout << "three"<<endl;
+    }
+    else if(i == 4) {
+        cout << "four"<<endl;
+    }
+    else if(i == 5) {
+        cout << "five"<<endl;
+    }
+    else if(i == 6) {
+        cout << "six"<<endl;
+    }
+    else if(i == 7) {
+        cout << "seven"<<endl;
+    }
+    else if(i == 8) {
+        cout << "eight"<<endl;
+    }
+    else if(i == 9) {
+        cout << "nine"<<endl;
+    }
+    else if(i>9){
+        if(i%2==0){
+            cout<<"even"<<endl;
+        }
+        else {
+            cout<<"odd"<<endl;
+        }
+    }
+    }
+    return 0;
+}
